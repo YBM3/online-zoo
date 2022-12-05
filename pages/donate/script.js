4f2ad5b43b52fc215aee25242e318a97af63a21e
YBM3 <102541929+YBM3@users.noreply.github.com>
2022-12-05 21:23:46 +0300

    feat: add amount
    add amount

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -20,3 +20,50 @@ function renderPopup(){
     popup.appendChild(menu);
     popup.appendChild(design); 
 }
+/////////////////////////////////////////////////////////////////////////////
+function limit(element) {
+    var max_chars = 4;
+  
+    if (element.value.length > max_chars) {
+      element.value = element.value.substr(0, max_chars);
+    }
+  }
+  
+  const donateInput = document.querySelector('.donate-input');
+  const amount = document.querySelectorAll('.progress-bar-input');
+  const donateForm = document.querySelector('.donate-form');
+  
+  function limitCharacter() {
+    if (donateInput.value.length > 4) {
+      donateInput.value = donateInput.value.slice(0, 4);
+    }
+    donateInput.value = donateInput.value.replace(/[e,+,-,',','.']/g, '');
+  }
+  
+  function checkedAmount() {
+    for (let i = 0; i < amount.length; i++) {
+      if (amount[i].checked) {
+        donateInput.value = amount[i].value;
+        break;
+      }
+    }
+  }
+  
+  function checkedDonateInput() {
+    for (let i = 0; i < amount.length; i++) {
+      if (donateInput.value === amount[i].value) {
+        amount[i].checked = true;
+      } else {
+        amount[i].checked = false;
+      }
+    }
+  }
+  
+  function defaultInput() {
+    donateInput.value = amount[5].value;
+  }
+  
+  donateInput.addEventListener('input', checkedDonateInput);
+  donateForm.addEventListener('click', checkedAmount);
+  donateInput.addEventListener('input', limitCharacter);
+  window.addEventListener('load', defaultInput);
